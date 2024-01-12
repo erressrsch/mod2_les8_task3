66a4328751f131c8696da64ba38dc929fec7a832
Szymon Chyc <erressrsch@gmail.com>
2024-01-12 22:59:09 +0100

    Add project files.

---
+++ mod2_les8_task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine($"F{i} = {c}");
+
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+}
